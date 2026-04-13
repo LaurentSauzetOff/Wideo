a74e4f38acc012c9ca5de7fb0af1d71fa7a04983
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-13 20:39:42 +0200

    wip : adding authentication

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -16,7 +16,14 @@ export const HomeNavbar = () => {
           <SidebarTrigger />
           <Link prefetch href="/" className="hidden md:block">
             <div className="p-4 flex items-center gap-1">
-              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                width={32}
+                height={32}
+                priority // Indispensable pour un logo (LCP)
+                unoptimized // On définit les deux
+              />
               <p className="text-xl font-semibold tracking-tight">Wideo</p>
             </div>
           </Link>
