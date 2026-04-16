a44daa061cb4f28db603588e14f3082fd691c6f7
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-16 14:23:24 +0200

    feat: configuration of trpc is ok

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -3,7 +3,7 @@
 import { trpc } from "@/trpc/client";
 
 export const PageClient = () => {
-  const [data] = trpc.hello.useQuery({ text: "Coucou" });
+  const { data } = trpc.hello.useQuery({ text: "Coucou" });
 
   return <div>Page client says : {data?.greeting}</div>;
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,15 @@
 import { z } from "zod";
-import { baseProcedure, createTRPCRouter } from "../init";
-import { TRPCError } from "@trpc/server";
+import { protectedProcedure, createTRPCRouter } from "../init";
+import { auth } from "@clerk/nextjs/server";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       }),
     )
     .query((opts) => {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "This is an error from the server",
-      });
+      console.log({ dbUser: opts.ctx.user });
       return {
         greeting: `hello ${opts.input.text}`,
       };
