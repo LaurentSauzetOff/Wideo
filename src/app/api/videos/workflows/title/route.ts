4bc42cbd2234a694f3c5aefd6e34befc2cfa390b
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-21 20:23:22 +0200

    feat(workflow): add title workflow db fetch and update steps

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -1,11 +1,39 @@
+import { db } from "@/db";
 import { serve } from "@upstash/workflow/nextjs";
+import { and, eq } from "drizzle-orm";
+import { videos } from "@/db/schema";
+
+interface InputType {
+  userId: string;
+  videoId: string;
+}
 
 export const { POST } = serve(async (context) => {
-  await context.run("initial-step", () => {
-    console.log("le premier step marche");
+  const input = context.requestPayload as InputType;
+  const { userId, videoId } = input;
+
+  const video = await context.run("get-video", async () => {
+    const [existingVideo] = await db
+      .select()
+      .from(videos)
+      .where(and(eq(videos.id, videoId), eq(videos.userId, userId)));
+
+    if (!existingVideo[0]) {
+      throw new Error("Video not found");
+    }
+
+    return existingVideo[0];
   });
 
-  await context.run("second-step", () => {
-    console.log("le second step marche");
+  console.log("existingVideo", video);
+
+  await context.run("update-video", async () => {
+    await db
+      .update(videos)
+      .set({ title: "updated from back" })
+      .where(and(eq(videos.id, video.id), eq(videos.userId, video.userId)));
   });
+
 });
+
+
